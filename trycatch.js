0ae47d2e733205fb61ca5ea25d06bfe3153d1849
V-Tamil <tamilselvi.venkatesan262@gmail.com>
2024-05-16 23:19:20 +0530

    conflict resolved

--- trycatch.js
+++ trycatch.js
@@ -1,6 +1,6 @@
-function sum(num1, num2) {
+function sum(num, num2) {
   try {
-    const sumvalue = num1 + num2;
+    const sumvalue = num + num2;
     return sumvalue;
   } catch (error) {
     console.log("error message", error);
